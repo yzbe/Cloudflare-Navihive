d76543710d66a53ce66f4a28f2090db92b8f2af3
yzbe <68817477+yzbe@users.noreply.github.com>
2026-04-05 01:57:54 +0800

    Update SiteCard.tsx

--- src/components/SiteCard.tsx
+++ src/components/SiteCard.tsx
@@ -94,7 +94,7 @@ const SiteCard = memo(function SiteCard({
     <Box
       sx={{
         height: 48, // 强行锁死卡片外部高度为 48px
-        width: 140, // 强行锁死卡片宽度为 140px，配合外部的流式布局
+        width: 120, // 强行锁死卡片宽度为 140px，配合外部的流式布局
         position: 'relative',
         transition: 'transform 0.3s ease-in-out',
         ...(!isEditMode && {
