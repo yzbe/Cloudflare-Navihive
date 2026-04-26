3f1460df017d1ef26df7ab3cd75aafd176d2fd88
yzbe <68817477+yzbe@users.noreply.github.com>
2026-04-26 17:31:18 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -95,7 +95,7 @@ const DEFAULT_CONFIGS = {
   'site.backgroundImage': '', // 背景图片URL
   'site.backgroundOpacity': '0.15', // 背景蒙版透明度
   // 'site.iconApi': 'https://www.faviconextractor.com/favicon/{domain}?larger=true', //原始图标方案
-  'site.iconApi': 'https://favicon.duowenit.com/favicon/{domain}', // 百度图标方案
+  'site.iconApi': 'https://favicon.duowenit.com/favicon/{domain}?larger=true',  // 百度图标方案
   'site.searchBoxEnabled': 'true', // 是否启用搜索框
   'site.searchBoxGuestEnabled': 'true', // 访客是否可以使用搜索框
 };
