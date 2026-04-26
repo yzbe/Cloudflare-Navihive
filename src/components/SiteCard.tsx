ac878fccc3b5f759d4e0a504b400c3879142c606
yzbe <68817477+yzbe@users.noreply.github.com>
2026-04-26 20:54:22 +0800

    Update SiteCard.tsx

--- src/components/SiteCard.tsx
+++ src/components/SiteCard.tsx
@@ -89,7 +89,7 @@ const SiteCard = memo(function SiteCard({
         transition: 'transform 0.3s ease-in-out',
         ...(!isEditMode && {
           '&:hover': {
-           transform: 'translateY(-2px)', 
+           transform: 'translateY(-20px)', 
           },
         }),
       }}
